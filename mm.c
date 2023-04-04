fff4e7465992a366264035cd2656350f92e077b2
masonchester <masonch32@gmail.com>
2023-04-04 10:47:04 -0400

    Defined the struct and methods and stdlib

--- mm.c
+++ mm.c
@@ -1,34 +1,42 @@
-typedef struct node{
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct node
+{
   unsigned int base_address;
   unsigned int limit_offeset;
 
-  struct node* next;
-  struct node* prev;
+  struct node *next;
+  struct node *prev;
+};
+
+struct node *head = NULL;
+struct node *tail = NULL;
+struct node *current = NULL;
+
+struct node *node_find(struct node *list, int search_address)
+{
 }
 
-struct node* head = NULL;
-struct node* tail = NULL;
-struct node* current = NULL;
-
-
-struct node* node_find(node* list, int search_address){
+void sort_by_id(struct node *list)
+{
 }
 
-void sort_by_id(node* list){
+void sort_by_size(struct node *list)
+{
 }
 
-void sort_by_size(node* list){
+void add(struct node *head, unsigned int base_address, unsigned int limit_offset)
+{
 }
 
-void add(node* head, unsigned int base_address, unsigned int limit_offset){
+void add(struct node *head, struct node *new_node)
+{
 }
 
-void add(node * head, node* new_node){
+void delete_by_address(struct node *head, int delete_id)
+{
 }
 
-void delete_by_address(node* head, int delete_id){
+void resize(struct node *head, unsigned int base_address, unsigned int limit_offset)
+{
 }
-
-void resize(node* head, unsigned int base_address, unsigned int limit_offset){
-}
-
