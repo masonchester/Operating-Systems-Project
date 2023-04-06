0a9a1b7258fe3c359bcf2920b4bc18ae7ec689df
masonchester <masonch32@gmail.com>
2023-04-06 15:18:55 -0400

    implement sory by limit offset

--- mm.c
+++ mm.c
@@ -13,6 +13,7 @@ typedef struct node
 struct node *head = NULL;
 struct node *tail = NULL;
 
+// searchs for the node which contains the current base address
 struct node *node_find(struct node *list, int search_address)
 {
   struct node *current = list;
@@ -23,6 +24,7 @@ struct node *node_find(struct node *list, int search_address)
   return current;
 }
 
+// sorts the list by the base address in increasing order
 void sort_by_baseaddress(struct node *list)
 {
   struct node *current = list;
@@ -45,8 +47,29 @@ void sort_by_baseaddress(struct node *list)
     current = current->next;
   }
 }
+
+// sorts the list by the limit offset by increasing order
 void sort_by_limitaddress(struct node *list)
 {
+  struct node *current = list;
+  while (current != NULL)
+  {
+    struct node *current_next = current->next;
+    while (current_next != NULL)
+    {
+      if (current_next->limit_offeset > current->limit_offeset)
+      {
+        unsigned int temp_base = current->base_address;
+        unsigned int temp_limit = current->limit_offeset;
+        current->base_address = current_next->base_address;
+        current->limit_offeset = current_next->limit_offeset;
+        current_next->base_address = temp_base;
+        current_next->limit_offeset = temp_limit;
+      }
+      current_next = current_next->next;
+    }
+    current = current->next;
+  }
 }
 
 void add(struct node *head, unsigned int base_address, unsigned int limit_offset)
