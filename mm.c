4006218903b6f83f46b6ee46056404a917ee2ade
MikeG86 <michaelgrunk@gmail.com>
2023-04-13 21:18:16 -0400

    Holes list is functional and working

--- mm.c
+++ mm.c
@@ -45,30 +45,6 @@ void sort_by_base_address(struct node *list)
   }
 }
 
-// sorts the list by the limit offset by increasing order
-void sort_by_limit_address(struct node *list)
-{
-  struct node *current = list;
-  while (current != NULL)
-  {
-    struct node *current_next = current->next;
-    while (current_next != NULL)
-    {
-      if (current_next->limit_offeset < current->limit_offeset)
-      {
-        unsigned int temp_base = current->base_address;
-        unsigned int temp_limit = current->limit_offeset;
-        current->base_address = current_next->base_address;
-        current->limit_offeset = current_next->limit_offeset;
-        current_next->base_address = temp_base;
-        current_next->limit_offeset = temp_limit;
-      }
-      current_next = current_next->next;
-    }
-    current = current->next;
-  }
-}
-
 // add a node in its sorted position
 void add(struct node **head, unsigned int base_address, unsigned int limit_offset)
 {
@@ -86,22 +62,42 @@ void add(struct node **head, unsigned int base_address, unsigned int limit_offse
     return;
   }
   struct node *current = *head;
-  // search the list until you find a base address current base address smaller than the new nodes
-  while (current->next != NULL && current->next->base_address <= base_address)
+  if (current->base_address > base_address)
   {
-    current = current->next;
-  }
+    if (current->next != NULL)
+    {
 
-  // if the current address -> next is null than you are the last node
-  if (current->next != NULL)
+      current->next->prev = new_node;
+    }
+
+    new_node->next = current->next;
+    current->next = new_node;
+    new_node->prev = current;
+
+    new_node->base_address = current->base_address;
+    new_node->limit_offeset = current->limit_offeset;
+    current->base_address = base_address;
+    current->limit_offeset = limit_offset;
+  }
+  else
   {
-    current->next->prev = new_node;
-  }
+    // search the list until you find a base address current base address smaller than the new nodes
+    while (current->next != NULL && current->next->base_address < base_address)
+    {
+      current = current->next;
+    }
 
-  // link current node and new node
-  new_node->next = current->next;
-  current->next = new_node;
-  new_node->prev = current;
+    // if the current address -> next is null than you are the last node
+    if (current->next != NULL)
+    {
+      current->next->prev = new_node;
+    }
+
+    // link current node and new node
+    new_node->next = current->next;
+    current->next = new_node;
+    new_node->prev = current;
+  }
 }
 
 void delete_by_address(struct node **head, unsigned int delete_address)
@@ -130,10 +126,127 @@ void delete_by_address(struct node **head, unsigned int delete_address)
     current->next->prev = current->prev;
   }
   free(current); // deallocate current
-  return;
 }
 
 void resize(struct node **head, unsigned int base_address, unsigned int limit_offset)
 {
+  struct node *temp = *head;
+  while (temp->next != NULL && temp->base_address != base_address)
+  {
+    temp = temp->next;
+  }
+  if (temp->next != NULL)
+  {
+    temp->limit_offeset = limit_offset;
+    sort_by_base_address(temp);
+  }
 }
 
+void create_holes_list(struct node **holes_list,struct node **memoryList,unsigned int base_address, unsigned int limit_offset)
+{
+  struct node *temp_memory = *memoryList;
+  struct node *temp_hole = *holes_list;
+  if(temp_memory == NULL)
+  {
+    add(holes_list,base_address,limit_offset);
+    return;
+  }
+  else if(temp_memory->next == NULL)
+  {
+    if(temp_hole == NULL)
+    {
+      add(holes_list,temp_memory->limit_offeset+1,limit_offset);
+    }
+    else
+    {
+    temp_hole->base_address = temp_memory->limit_offeset + 1;
+    temp_hole->limit_offeset = limit_offset;
+    }
+    return;
+  }
+  else if(temp_memory != NULL)
+  {
+    while(temp_memory->next != NULL)
+    {
+      if(temp_memory->base_address + temp_memory->limit_offeset + 1 < temp_memory->next->base_address-1)
+      {
+        add(holes_list,(temp_memory->limit_offeset + 1),temp_memory->next->base_address-1);
+      }
+      temp_memory = temp_memory->next;
+    }
+    if(temp_memory != NULL)
+    {
+      add(holes_list,temp_memory->limit_offeset +1, limit_offset);
+    }
+  }
+}
+void delete_hole_list(node **head)
+{
+  node *current = *head;
+  node *next_node;
+  while(current != NULL)
+  {
+    next_node = current->next;
+    free(current);
+    current = next_node;
+  }
+  *head = NULL;
+}
+void print_hole_list(node* the_list)
+{
+  node* current = the_list;
+  while(current != NULL)
+  {
+    printf("%d", current->base_address);
+    printf(" ");
+    printf("%d",current->limit_offeset);
+    printf(" ");
+    current = current->next;
+  }
+}
+
+
+void main()
+{
+  struct node *head = NULL;
+  struct node *hole_list = NULL;
+create_holes_list(&hole_list,&head,0,1024);
+ printf("holes ");
+ print_hole_list(hole_list);
+printf("\n");
+
+add(&head,0, 200);
+ printf("Memory ");
+ print_hole_list(head);
+ printf("\n");
+ create_holes_list(&hole_list,&head,0,1024);
+ printf("holes ");
+ print_hole_list(hole_list);
+ printf("\n");
+delete_hole_list(&hole_list);
+
+ add(&head,300, 401);
+ printf("Memory ");
+ print_hole_list(head);
+ printf("\n");
+ create_holes_list(&hole_list,&head,0,1024);
+ printf("hole_list ");
+ print_hole_list(hole_list);
+ printf("\n");
+ delete_hole_list(&hole_list);
+
+
+printf("Memory ");
+ add(&head,800, 1000);
+ print_hole_list(head);
+ printf("\n");
+ printf("Hole_list ");
+ create_holes_list(&hole_list,&head,0,1024);
+ print_hole_list(hole_list);
+
+  //delete_hole_list(&hole_list);
+  //add(&head, 201, 300);
+  //create_holes_list(&hole_list,&head,0,0x400);
+  //print_hole_list(&hole_list);
+  
+}
